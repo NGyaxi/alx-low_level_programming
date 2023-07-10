eb76bf2b126b7e536a687a97eab24a117172eec4
NGyaxi <fredrickgyasi@gmail.com>
2023-07-09 17:59:35 -0700

    static lib

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,19 +1,16 @@
-#include "main.h"
-
+#include "holberton.h"
 /**
- * _puts - prints a string, followed by a new line,
- * @str: pointer to the string to print
- * Return: void
+ * _puts - Check if a number is greater than 0
+ * @str: The number to be checked
  */
-
 void _puts(char *str)
 {
-	int i = 0;
+	int i;
 
-	while (str[i])
+	for (i = 0; *str != '\0'; i++)
 	{
-		_putchar(str[i]);
-		i++;
+		_putchar(*str);
+		str++;
 	}
 	_putchar('\n');
 }
