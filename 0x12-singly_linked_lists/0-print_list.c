fd7dece5e3a4341d3d1040ca684278eb3a024945
NGyaxi <fredrickgyasi@gmail.com>
2023-07-28 00:21:40 -0700

    0x12-singly_linked_lists

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,23 +1,41 @@
-#include "lists.h"
-/**
- * print_list - prints all the elements of a list_t list.
- * @h: singly linked list.
- * Return: number of elements in the list.
- */
+#include <stdio.h>
+#include "list.h"
 
+/**
+ * print_list - Print all elements of a linked list
+ * @h: Pointer to the head node of the list
+ *
+ * Return: The number of nodes in the list
+ */
 size_t print_list(const list_t *h)
 {
-	size_t nelem;
+	size_t count = 0;
 
-	nelem = 0;
 	while (h != NULL)
 	{
-		if (h->str == NULL)
-			printf("[%d] %s\n", 0, "(nil)");
+		if (h->str != NULL)
+			printf("%s\n", h->str);
 		else
-			printf("[%d] %s\n", h->len, h->str);
+			printf("[0] (nil)\n");
+
 		h = h->next;
-		nelem++;
+		count++;
 	}
-	return (nelem);
+
+	return count;
+}
+
+int main(void)
+{
+	/* Example usage: */
+	list_t node3 = { "World", NULL };
+	list_t node2 = { "Hello", &node3 };
+	list_t node1 = { "Linked", &node2 };
+	list_t node0 = { "This", &node1 };
+	list_t *head = &node0;
+
+	size_t num_nodes = print_list(head);
+	printf("Number of nodes: %zu\n", num_nodes);
+
+	return 0;
 }
--- 0x12-singly_linked_lists/list.h
+++ 0x12-singly_linked_lists/list.h
@@ -1,30 +1,21 @@
-#ifndef _LISTS_
-#define _LISTS_
+#ifndef LIST_H
+#define LIST_H
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#include <stddef.h>
 
 /**
- * struct list_s - singly linked list
+ * struct list_t - singly linked list
  * @str: string - (malloc'ed string)
- * @len: length of the string
  * @next: points to the next node
  *
  * Description: singly linked list node structure
- * for ALX project
  */
-typedef struct list_s
+typedef struct list_t
 {
 	char *str;
-	unsigned int len;
-	struct list_s *next;
+	struct list_t *next;
 } list_t;
 
 size_t print_list(const list_t *h);
-size_t list_len(const list_t *h);
-list_t *add_node(list_t **head, const char *str);
-list_t *add_node_end(list_t **head, const char *str);
-void free_list(list_t *head);
 
-#endif
+#endif /* LIST_H */
